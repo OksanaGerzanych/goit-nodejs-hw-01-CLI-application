e8f21ec5ff57a270cd3ec1d000a3c24a0f459096
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2023-05-04 10:40:48 +0300

    Delete file node_modules

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 const contacts = require("./contacts");
-const { Command } = require("commander");
-const program = new Command();
+// const { Command } = require("commander");
+// const program = new Command();
 
-const invokeAction = async ({ action, id, name, email, phone }) => {
-  switch (action) {
-    case "list":
-      const listAllContacts = await contacts.listContacts();
-      return console.table(listAllContacts);
+// const invokeAction = async ({ action, id, name, email, phone }) => {
+//   switch (action) {
+//     case "list":
+//       const listAllContacts = await contacts.listContacts();
+//       return console.table(listAllContacts);
 
-    case "get":
-      const contactById = await contacts.getContactById(id);
-      return console.log(contactById);
+//     case "get":
+//       const contactById = await contacts.getContactById(id);
+//       return console.log(contactById);
 
-    case "add":
-      const newContact = await contacts.addContact(name, email, phone);
-      return console.log(newContact);
+//     case "add":
+//       const newContact = await contacts.addContact(name, email, phone);
+//       return console.log(newContact);
 
-    case "remove":
-      const deleteContact = await contacts.removeContact(id);
-      return console.log(deleteContact);
-    default:
-      console.warn("\x1B[31m Unknown action type!");
-  }
-};
+//     case "remove":
+//       const deleteContact = await contacts.removeContact(id);
+//       return console.log(deleteContact);
+//     default:
+//       console.warn("\x1B[31m Unknown action type!");
+//   }
+// };
 
-program
-  .option("-a, --action <type>", "choose action")
-  .option("-i, --id <type>", "user id")
-  .option("-n, --name <type>", "user name")
-  .option("-e, --email <type>", "user email")
-  .option("-p, --phone <type>", "user phone");
+// program
+//   .option("-a, --action <type>", "choose action")
+//   .option("-i, --id <type>", "user id")
+//   .option("-n, --name <type>", "user name")
+//   .option("-e, --email <type>", "user email")
+//   .option("-p, --phone <type>", "user phone");
 
-program.parse(process.argv);
+// program.parse(process.argv);
 
-const argv = program.opts();
+// const argv = program.opts();
 
-invokeAction(argv);
+// invokeAction(argv);
